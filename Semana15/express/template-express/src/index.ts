8a1a33d1c230d488e4b5b98461102a02166c4533
unknown <claricepassos@gmail.com>
2021-09-26 13:45:14 -0300

    express

--- Semana15/express/template-express/src/index.ts
+++ Semana15/express/template-express/src/index.ts
@@ -22,17 +22,7 @@ app.use(cors());
 
 const server = app.listen(3003, () => console.log(countries)) */
 
- app.get('/countries/:id', (req: Request, res: Response) => {
-    const result = countries.find(country => {
-        return Number(country.id) === Number(req.params.id)
-    })
-    if (result) {
-        res.status(200).send(result)
-    } else {
-        res.status(404).send("Not found")
-    }
-})
- 
+
 app.get('/countries/search', (req: Request, res: Response) => {
     let result: country[] = countries
     if (req.query.name) {
@@ -60,15 +50,38 @@ app.get('/countries/search', (req: Request, res: Response) => {
     }
 })
 
-app.put('/countries/:id',  (req: Request, res: Response) => {
-    const result = {name: "",capital: ""} 
-    if (result) {
-        res.status(200).send(result)
-    } else {
-        res.status(404).send("Not found")
-    }
-})
-
+    app.post("/countries/:id", (req: Request, res: Response) => {
+        let errorCode: number = 400;
+        //inicio de um sonho
+        try {
+          const countryIndex: number = countries.findIndex(
+            (country) => country.id === Number(req.params.id)
+          );
+      
+          if (countryIndex === -1) {
+            errorCode = 404;
+            throw new Error();
+          }
+      
+          if (!req.body.name && !req.body.capital) {
+            throw new Error("Invalid Parameters");
+          }
+      
+          if (req.body.name) {
+            countries[countryIndex].name = req.body.name;
+          }
+          if (req.query.capital) {
+            countries[countryIndex].capital = req.body.capital;
+          }
+      
+          //deu tudo certo
+          res.status(200).send("Country successfully updated");
+        } catch (error) {
+          //deu tudo errado
+          console.log(error);
+          res.status(errorCode).send(error.message);
+        }
+      });
 
 app.listen(3003, () => {
     console.log("Server is online at http://localhost:3003.")
