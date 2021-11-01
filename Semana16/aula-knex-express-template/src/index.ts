912c0b05a6bf79e2df43063a58c7d7b4fbd778c3
unknown <claricepassos@gmail.com>
2021-11-01 15:17:10 -0300

    get post by id feito

---
+++ Semana16/aula-knex-express-template/src/index.ts
@@ -0,0 +1,78 @@
+import express, { Express } from "express";
+import knex from "knex";
+import cors from "cors";
+import dotenv from "dotenv";
+import { AddressInfo } from "net";
+import { Request, Response } from "express"
+
+dotenv.config();
+
+export const connection = knex({
+	client: "mysql",
+	connection: {
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_SCHEMA
+  }
+});
+
+const app: Express = express();
+app.use(express.json());
+app.use(cors());
+
+const getActorById = async (id: string): Promise<any> => {
+  const result = await connection.raw(`
+    SELECT * FROM Actor WHERE id = '${id}'
+  `)
+
+	return result[0][0]
+}
+
+app.get("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id
+
+    console.log(await getActorById(id))
+
+    res.end()
+  } catch (error: any) {
+		console.log(error.message)
+    res.status(500).send("Unexpected error")
+  }
+})
+
+const countActors = async (gender: string): Promise<any> => {
+  const result = await connection.raw(`
+    SELECT COUNT(*) as count FROM Actor WHERE gender = "${gender}"
+  `);
+	// Só colocamos esse "as count" como apelido, para ficar mais fácil de pegar
+	// o valor no resultado!
+  const count = result[0][0].count;
+  return count;
+};
+
+app.get("/actor/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const actor = await getActorById(id);
+
+    res.status(200).send(actor)
+  } catch (err: any) {
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+});
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+       const address = server.address() as AddressInfo;
+       console.log(`Server is running in http://localhost: ${address.port}`);
+    } else {
+       console.error(`Failure upon starting server.`);
+    }
+});
+
+
---
+++ Semana20/Cookenu/src/endpoints/getRecepieById.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express';
+import findRecepieById from '../data/findRecepieById'
+import { login } from './login';
+
+export default async function getRecepieById(
+    req: Request,
+    res: Response
+
+) {
+    try {
+        const recepie = await findRecepieById(req.params.id)
+
+        if (!recepie) {
+            res.status(404).send({
+                message: 'Receita não encontrado'
+            })
+        }
+
+
+        const token = req.headers.authorization
+
+        const getData = new Authenticator().getTokenData(token)
+
+        if(!getData){
+            res.status(409).send({
+                message: 'Usuário não autorizado'
+            })
+        }
+        res.status(200).send({
+            message: 'Sucesso!!',
+            recepie
+        })
+
+
+    } catch (error: any) {
+        res.status(400).send({
+            message: error.message || error.sqlMessage
+        })
+    }
+}
+
+
