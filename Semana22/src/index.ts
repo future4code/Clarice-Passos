a5daa6978574a84146908e35b81ad3cd1f68e6e0
unknown <claricepassos@gmail.com>
2021-11-06 19:51:39 -0300

    adicionar pokemon feito

--- Semana22/src/index.ts
+++ Semana22/src/index.ts
@@ -1,7 +1,8 @@
 import { app } from './app'
 import getAllPokemons from './endpoints/getAllPokemons'
+import postPokemon from './endpoints/postPokemon'
 
-//app.post('/pokemon/post', createPokemon)
+app.post('/pokemon', postPokemon)
 app.get ('/pokemon', getAllPokemons)
 //app.get('/pokemon/:id', getPokemonByID)
 
