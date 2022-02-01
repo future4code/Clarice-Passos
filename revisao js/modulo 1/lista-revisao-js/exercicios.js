2ff016c56462b986e6df5cf64438ef6daa1a8497
unknown <claricepassos@gmail.com>
2022-02-01 13:49:12 -0300

    exercicio 15

--- revisao js/modulo 1/lista-revisao-js/exercicios.js
+++ revisao js/modulo 1/lista-revisao-js/exercicios.js
@@ -167,7 +167,12 @@ function retornaContasComSaldoAtualizado(contas) {
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-
+  const sorted = consultas.sort(function (a, b) {
+	
+      return (a.nome > b.nome) ? 1 : ((b.nome > a.nome) ? -1 : 0);
+    
+   });
+    return sorted
 }
 
 // EXERCÍCIO 15B
