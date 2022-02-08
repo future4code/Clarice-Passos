1eff99de6410b0eed26c53c1e7a0c5e133a7db2b
unknown <claricepassos@gmail.com>
2022-02-08 17:28:49 -0300

    exercicio 5

--- revisao/star-wars-marzo/site-sw/src/pages/CharacterDetailPage/CharacterDetailPage.js
+++ revisao/star-wars-marzo/site-sw/src/pages/CharacterDetailPage/CharacterDetailPage.js
@@ -7,12 +7,12 @@ import { useParams } from "react-router-dom"
 const CardViagem = styled.div`
 border: solid 1px red;
 width: 300px;
-height: 250px;
+height: 300px;
 margin-right: auto;
 margin-left: auto;
 margin-bottom: 10px;
 align: center;
-margin-top: 100px ;
+margin-top: 100px;
 `
 const Conteiner = styled.div`
 background-color: #ffe8d6;
@@ -32,28 +32,40 @@ const CharacterDetailPage = () => {
   }
 
   const [detailsUrl, setDetailsUrl] = useState({})
+  const [homeworldLink, setHomeworldLink] = useState("");
+  const [homeworld, setHomeworld] = useState("");
 
   const getCharacterUrl = (id) => {
     axios
       .get(`https://swapi.dev/api/people/${params.id}`)
       .then((r) => {
         setDetailsUrl(r.data)
-        console.log(r.data)
+        setHomeworldLink(r.data.homeworld)
       })
       .catch((err) => {
         console.log(err.response)
       })
   }
 
+
   useEffect(() => {
     getCharacterUrl()
   }, [])
 
+  useEffect(() => {
+    homeworldLink &&
+      axios.get(homeworldLink).then((response) => {
+        setHomeworld(response.data);
+      });
+  }, [homeworldLink]);
+
   return (
     <Conteiner>
       <CardViagem>
-        <h1>Nome:</h1> <p>{detailsUrl.name}</p>
+        <h2>Nome:</h2> <p>{detailsUrl.name}</p>
         <h3>URL:</h3> <p>{detailsUrl.url}</p>
+        <h3>Origem:</h3> <p>{homeworld.name}</p>
+
         <button onClick={voltar}>Voltar</button>
       </CardViagem>
     </Conteiner>
